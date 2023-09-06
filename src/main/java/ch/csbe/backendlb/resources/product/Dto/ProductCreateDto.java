dd0287d7f9172af36f3ec2bcbcf2317e0d71b09c
Rytryx <imrytry@gmail.com>
2023-09-06 15:19:09 +0200

    Completing Product-Dto's and restrukturing the Folder

---
+++ src/main/java/ch/csbe/backendlb/resources/product/Dto/ProductCreateDto.java
@@ -0,0 +1,27 @@
+package ch.csbe.backendlb.resources.product.Dto;
+
+import ch.csbe.backendlb.resources.category.Category;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCreateDto {
+    @NotNull
+    private Long id;
+    private String sku;
+    @NotNull
+    private String name;
+    private String image;
+    private String description;
+    @NotNull
+    private Double price;
+    private Integer stock;
+    private Category category;
+
+}
