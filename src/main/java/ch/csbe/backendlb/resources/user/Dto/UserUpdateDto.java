4e877ef97d5f34090c00412a257ced59032b2dd2
Rytryx <imrytry@gmail.com>
2023-09-06 15:23:52 +0200

    Completing User-Dto's and restrukturing the Folder

---
+++ src/main/java/ch/csbe/backendlb/resources/user/Dto/UserUpdateDto.java
@@ -0,0 +1,31 @@
+package ch.csbe.backendlb.resources.user.Dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateDto {
+    @NotNull
+    private Long id;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    private String surname;
+    private String lastname;
+    private String email;
+    private Date birthday;
+    private String streetName;
+    private String streetNr;
+    private String zipCode;
+    private String city;
+}
+
