f0c3ba1c74c0422546ccb4dd29e8c361bda12f34
Rytryx <imrytry@gmail.com>
2023-09-06 16:32:09 +0200

    Saving Progress of implementing Dto's into my Code

--- src/main/java/ch/csbe/backendlb/resources/category/CategoryServiceImpl.java
+++ src/main/java/ch/csbe/backendlb/resources/category/CategoryServiceImpl.java
@@ -1,37 +1,44 @@
 package ch.csbe.backendlb.resources.category;
+
+import ch.csbe.backendlb.resources.category.Dto.CategoryCreateDto;
+import ch.csbe.backendlb.resources.category.Dto.CategoryShowDto;
+import ch.csbe.backendlb.resources.category.Dto.CategoryDetailDto;
+import ch.csbe.backendlb.resources.category.Dto.CategoryUpdateDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import ch.csbe.backendlb.resources.category.CategoryService;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-
-
     @Autowired
     CategoryRepository categoryRepository;
 
-
-
-    public List<Category> getAll() {
-        return this.categoryRepository.findAll();
+    public List<CategoryShowDto> getAll() {
+        List<Category> categories = this.categoryRepository.findAll();
+        return categories.stream().map(Category::toCategoryShowDto).collect(Collectors.toList());
     }
 
-    public Category create(Category categoryToSave) {
-        return this.categoryRepository.save(categoryToSave);
+    public CategoryShowDto create(CategoryCreateDto categoryCreateDto) {
+        Category categoryToSave = Category.fromCategoryCreateDto(categoryCreateDto);
+        Category savedCategory = this.categoryRepository.save(categoryToSave);
+        return savedCategory.toCategoryShowDto();
     }
 
-    public Category getById(Long id) {
-        return this.categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
+    public CategoryDetailDto getById(Long id) {
+        Category category = this.categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
+        return category.toCategoryDetailDto();
     }
 
-    public Category update(Long id, Category categoryToUpdate) {
-        Category category = this.categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
-        category.setName(categoryToUpdate.getName());
-        return categoryRepository.save(category);
+    public CategoryShowDto update(Long id, CategoryUpdateDto categoryUpdateDto) {
+        Category category = this.categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
+        category.updateFromCategoryUpdateDto(categoryUpdateDto);
+        Category updatedCategory = categoryRepository.save(category);
+        return updatedCategory.toCategoryShowDto();
     }
 
     public void delete(Long id) {
