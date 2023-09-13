3a04952ef1a6860a102be5460dc9d087de05b7dc
Rytryx <imrytry@gmail.com>
2023-09-13 09:48:10 +0200

    Documenting Dto's of Product

--- src/main/java/ch/csbe/backendlb/resources/product/Dto/ProductDetailDto.java
+++ src/main/java/ch/csbe/backendlb/resources/product/Dto/ProductDetailDto.java
@@ -6,21 +6,38 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO für die Detailansicht eines Produkts")
 public class ProductDetailDto {
     @NotNull
+    @Schema(description = "Einzigartige Identifikationsnummer des Produkts")
     private Long id;
-    private String sku;
-    @NotNull
-    private String name;
-    private String image;
-    private String description;
-    @NotNull
-    private Double price;
-    private Integer stock;
-    private Category category;
 
+    @Schema(description = "Artikelnummer (SKU) des Produkts")
+    private String sku;
+
+    @NotNull
+    @Schema(description = "Name des Produkts")
+    private String name;
+
+    @Schema(description = "URL des Produktbilds")
+    private String image;
+
+    @Schema(description = "Beschreibung des Produkts")
+    private String description;
+
+    @NotNull
+    @Schema(description = "Preis des Produkts")
+    private Double price;
+
+    @Schema(description = "Lagerbestand des Produkts")
+    private Integer stock;
+
+    @Schema(description = "Kategorie des Produkts")
+    private Category category;
 }
--- src/main/java/ch/csbe/backendlb/resources/product/Dto/ProductUpdateDto.java
+++ src/main/java/ch/csbe/backendlb/resources/product/Dto/ProductUpdateDto.java
@@ -6,22 +6,38 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO für das Aktualisieren eines Produkts")
 public class ProductUpdateDto {
     @NotNull
+    @Schema(description = "Einzigartige Identifikationsnummer des Produkts")
     private Long id;
-    private String sku;
-    @NotNull
-    private String name;
-    private String image;
-    private String description;
-    @NotNull
-    private Double price;
-    private Integer stock;
-    private Category category;
 
+    @Schema(description = "Artikelnummer (SKU) des Produkts")
+    private String sku;
+
+    @NotNull
+    @Schema(description = "Neuer Name des Produkts")
+    private String name;
+
+    @Schema(description = "URL des neuen Produktbilds")
+    private String image;
+
+    @Schema(description = "Neue Beschreibung des Produkts")
+    private String description;
+
+    @NotNull
+    @Schema(description = "Neuer Preis des Produkts")
+    private Double price;
+
+    @Schema(description = "Neuer Lagerbestand des Produkts")
+    private Integer stock;
+
+    @Schema(description = "Neue Kategorie des Produkts")
+    private Category category;
 }
