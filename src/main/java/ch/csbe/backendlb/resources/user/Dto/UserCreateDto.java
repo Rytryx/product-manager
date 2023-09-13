3a0b01c12ff59e2c620b0d3f22186436f67080d6
Rytryx <imrytry@gmail.com>
2023-09-13 09:50:36 +0200

    Documenting Dto's of User

--- src/main/java/ch/csbe/backendlb/resources/user/Dto/UserCreateDto.java
+++ src/main/java/ch/csbe/backendlb/resources/user/Dto/UserCreateDto.java
@@ -6,25 +6,46 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.util.Date;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO für das Erstellen eines Benutzers")
 public class UserCreateDto {
     @NotNull
+    @Schema(description = "Einzigartige Identifikationsnummer des Benutzers")
     private Long id;
+
     @NotNull
+    @Schema(description = "Benutzername")
     private String username;
+
     @NotNull
+    @Schema(description = "Passwort")
     private String password;
+
+    @Schema(description = "Nachname")
     private String surname;
+
+    @Schema(description = "Vorname")
     private String lastname;
+
+    @Schema(description = "E-Mail-Adresse")
     private String email;
+
+    @Schema(description = "Geburtstag")
     private Date birthday;
+
+    @Schema(description = "Straßenname")
     private String streetName;
+
+    @Schema(description = "Hausnummer")
     private String streetNr;
+
+    @Schema(description = "Postleitzahl")
     private String zipCode;
+
+    @Schema(description = "Stadt")
     private String city;
 }
-
